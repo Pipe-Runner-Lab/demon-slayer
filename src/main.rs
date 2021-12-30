96d1e7fd55f03d25fa1a33bb0afff5647cf6cde2
aakash_mallik <aakash.mallik@postman.com>
2021-12-30 14:26:50 +0530

    Initial game state setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.print(1, 1,"Hello bro");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Demon Slayer")
+        .build()?;
+
+    main_loop(context, State {})
 }
