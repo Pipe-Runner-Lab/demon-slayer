0dce696952d5854550e843e2ddb7e2ffe3a5ce65
aakash_mallik <aakash.mallik@postman.com>
2022-01-11 03:50:07 +0530

    Created map builder module

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 use bracket_lib::prelude::*;
 
 mod map;
+mod player;
+mod map_builder;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
+    pub use crate::map_builder::*;
 }
 
 use prelude::*;
 
 struct State {
     map: Map,
+    player: Player
 }
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new()
+            map: map_builder.map,
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+            )
         }
     }
 }
@@ -26,14 +36,16 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Demon Slayer")
-        .with_fps_cap(30)
+        .with_fps_cap(30.0)
         .build()?;
 
     main_loop(context, State::new())
